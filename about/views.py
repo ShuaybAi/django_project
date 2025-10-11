60e9e0dee692ad7a59f09c67abbe3ac1b12f14d8
ShuaybAi <aijaz.shuayb@gmail.com>
2025-10-11 14:15:00 +0100

    Your commit message

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def about_me(request):
+    """View function for the about page."""
     return HttpResponse("This would be the about page.")
